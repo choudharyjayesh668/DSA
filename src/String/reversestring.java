bea97ac62b63e5e09d207b3c7dcd7a7400e1c45d
Jayesh Choudhary <choudharyjayesh668@gmail.com>
2026-07-05 22:19:13 +0530

    Solved Reverse a String

---
+++ src/String/reversestring.java
@@ -0,0 +1,13 @@
+public class reversestring {
+    public static void reverseString(char[] arr){
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            char temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
